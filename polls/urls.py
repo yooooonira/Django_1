a67c087fd62528e0aef1480d3f6f9920581081c7
yooooonheera <yooooonira@gmail.com>
2025-06-04 14:05:11 +0900

    장고 공식문서 Part4

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,4 @@
+# view와 연결
 from django.urls import path
 from . import views #현재 위치의 views.py를 import해 온다. 
 # . == 현재 위치 (현재위치에 views.py가 없으면 만들면 됨)
@@ -13,11 +14,19 @@ from . import views #현재 위치의 views.py를 import해 온다.
 app_name = "polls" # 네임스페이스 지정
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    #detail은 숫자와 연결
-    path("<int:question_id>/results/", views.results, name="results"),
-    #result는 숫자/results/ 와 연결
+    # path("", views.index, name="index"),
+    # path("<int:question_id>/", views.detail, name="detail"),
+    # #detail은 숫자와 연결
+    # path("<int:question_id>/results/", views.results, name="results"),
+    # #result는 숫자/results/ 와 연결
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
+    # #전선의 개념으로 짝을 맞춰줘야 한다.
+
+
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    
+
 ]
-#전선의 개념으로 짝을 맞춰줘야 한다.
--- polls/views.py
+++ polls/views.py
@@ -1,65 +1,69 @@
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
 from django.shortcuts import render, get_object_or_404
 #404에러는 페이지가 없을때 생기는 에러
 from django.http import HttpResponse
 #http요청받을걸 뿌리고 싶다
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 
 
 # def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
-
-# #여긴 py파일이니까 html을 적는건 좋지 않아(따로 파일을 써서 연결을 해야지) 그런데 돌아가긴 하거든?
-# #그런데 먼저 연습용으로 써보려고 해 // 인터넷에 반환되는 걸 확인하기 위해서!
-# def index(request):
-#     html = """
-#     <html>
-#     <head>
-#         <title>투표 사이트</title>
-#         <style>
-#             body {
-#                 font-family: Arial, sans-serif;
-#                 background-color: #c6c6c6;
-#                 padding: 2em;
-#             }
-           
-#             h1 {color: #ffffff;}
-#             .box  {
-#             background: #b951a6;
-#             padding: 1.5em;
-#             border-radius: 28px;
-#             box-shadow: 0 0 10px rgb(0 0 0);
-#             max-width: 600px;
-#             margin: auto;
-#         }
-#         </style>
-#     </head>
-
-#     <body>
-#         <div class="box">
-#             <h1>안녕하세요!</h1>
-#             <p>여기는 <strong>설문조사(polls)</strong>사이트의 메인 페이지입니다.</p>
-#             <p>관리자는 설문을 추가하고, 사용자는 투표할 수 있습니다. </p>
-#         </div>
-#     </body>
-
-#     </html>
-#     """
-#     return HttpResponse(html)
-
-# index(최신글 list)
-def index(request):
 	
-	latest_question_list = Question.objects.order_by("-pub_date")[:5]
-	context = {"latest_question_list": latest_question_list}
-	return render(request, "polls/index.html", context)
+# 	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+# 	context = {"latest_question_list": latest_question_list}
+# 	return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+# 	try:
+# 		question = get_object_or_404(Question, pk=question_id)
+# 	except Question.DoesNotExist:
+# 		raise Htt404("Question does not exit")
+# 	return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
+# def vote(request, question_id):
+#     return HttpResponse(f"You're voting on question {question_id}.") 
+
+# 메인 페이지 (질문 목록)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+# 질문 상세 페이지
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    context_object_name = "question"
+
+# 결과 페이지
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    context_object_name = "question"
+
+# 투표 처리 로직
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.") 
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message":"You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
